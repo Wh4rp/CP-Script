4f3d5cecfca0114e98d20f59e6f842202484202e
Warp <matiasfernandeztaipe@gmail.com>
2022-05-23 23:10:20 -0400

    Fix bug of no such directory

--- CP.sh
+++ CP.sh
@@ -1,8 +1,10 @@
 function CP () {
 
-    cache_path="~/.cache"       # Directory where compiled files goes
+    cache_path="$HOME/.cache"   # Directory where compiled files goes
     input_path="./in.txt"       # Input file
     output_path="./out.txt"     # Output file
+    FILENAME="${1// /_}"
+    FILENAME="${FILENAME%.*}"   # File name without extension
     FORCE="false"
     DEFAULT="false"
     OK=0
@@ -23,18 +25,16 @@ function CP () {
     while [[ $1 == -* ]]; do
         shift
     done
-    
-    FILENAME="${1%.*}"
 
-    if [ ! -f $cache_path/$FILENAME.exe ] || [ $1 -nt $cache_path/$FILENAME.exe ] || [ "$FORCE" = "true" ];
+    if [ ! -f $cache_path/$FILENAME.exe ] || [ "${1}" -nt $cache_path/$FILENAME.exe ] || [ "$FORCE" = "true" ];
     then
         echo "Compiling ${1}..."
-        g++ -std=c++17 -g -O2 -Wshadow -Wall -Wextra -o "$cache_path/$FILENAME.exe" "$1"
+        g++ -std=c++17 -g -O2 -Wshadow -Wall -Wextra -o $cache_path/$FILENAME.exe "$1"
         OK=$?
     fi
 
     if [ -n "$2" ] && [ -n "$3" ]; then
-        (exit $OK) && echo "Running ${1}..." && printf '%s\n' -------------------- && $cache_path/$1.exe <"$2"> "$3"
+        (exit $OK) && echo "Running ${1}..." && printf '%s\n' -------------------- && $cache_path/$FILENAME.exe <"$2"> "$3"
     else
         if [ $DEFAULT = "true" ]; then
             (exit $OK) && echo "Running ${1}..." && printf '%s\n' -------------------- && time $cache_path/$FILENAME.exe <"$input_path"> "$output_path"
